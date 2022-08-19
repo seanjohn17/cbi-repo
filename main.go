b4d63fdb51ca96a2f16a10b815e802471bee492e
seanjohn17 <110308338+seanjohn17@users.noreply.github.com>
2022-08-19 18:51:46 -0500

    Update main.go

--- main.go
+++ main.go
@@ -372,7 +372,7 @@ func GetTaxiTrips(db *sql.DB) {
 
 	// Get the the Taxi Trips for Taxi medallions list
 
-	var url = "https://data.cityofchicago.org/resource/wrvz-psew.json?$limit=500"
+	var url = "https://data.cityofchicago.org/resource/wrvz-psew.json?$limit=1000"
 
 	tr := &http.Transport{
 		MaxIdleConns:          10,
@@ -404,7 +404,7 @@ func GetTaxiTrips(db *sql.DB) {
 
 	// Get the Taxi Trip list for rideshare companies like Uber/Lyft list
 	// Transportation-Network-Providers-Trips:
-	var url_2 = "https://data.cityofchicago.org/resource/m6dm-c72p.json?$limit=500"
+	var url_2 = "https://data.cityofchicago.org/resource/m6dm-c72p.json?$limit=1000"
 
 	res_2, err := http.Get(url_2)
 	if err != nil {
