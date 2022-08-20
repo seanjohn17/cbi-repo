c4d5e42c5c43c7cb6ac3b99fd9a17a58a8a0510f
seanjohn17 <110308338+seanjohn17@users.noreply.github.com>
2022-08-20 12:44:36 -0500

    Update main.go

--- main.go
+++ main.go
@@ -811,7 +811,7 @@ func GetBuildingPermits(db *sql.DB) {
 
 	// While doing unit-testing keep the limit value to 500
 	// later you could change it to 1000, 2000, 10,000, etc.
-	var url = "https://data.cityofchicago.org/resource/building-permits.json"
+	var url = "https://data.cityofchicago.org/resource/building-permits.json?$limit=10000"
 
 	tr := &http.Transport{
 		MaxIdleConns:       100,
