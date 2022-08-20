98ecbba55234fd4d00d39381d778e9b2e97d4b1a
seanjohn17 <110308338+seanjohn17@users.noreply.github.com>
2022-08-19 19:41:36 -0500

    Update main.go

--- main.go
+++ main.go
@@ -376,15 +376,15 @@ func GetTaxiTrips(db *sql.DB) {
 
 	tr := &http.Transport{
 		MaxIdleConns:          10,
-		IdleConnTimeout:       1000 * time.Second,
-		TLSHandshakeTimeout:   1000 * time.Second,
-		ExpectContinueTimeout: 1000 * time.Second,
+		IdleConnTimeout:       10000 * time.Second,
+		TLSHandshakeTimeout:   10000 * time.Second,
+		ExpectContinueTimeout: 10000 * time.Second,
 		DisableCompression:    true,
 		Dial: (&net.Dialer{
-			Timeout:   1000 * time.Second,
-			KeepAlive: 1000 * time.Second,
+			Timeout:   10000 * time.Second,
+			KeepAlive: 10000 * time.Second,
 		}).Dial,
-		ResponseHeaderTimeout: 1000 * time.Second,
+		ResponseHeaderTimeout: 10000 * time.Second,
 	}
 
 	client := &http.Client{Transport: tr}
